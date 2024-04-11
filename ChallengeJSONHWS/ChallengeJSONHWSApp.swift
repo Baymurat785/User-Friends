bd07ed74a58696fe0dc67f7dbcf0c64729daedfb
Baymurat785 <abdimuratovbaymurat03@gmail.com>
2024-04-11 09:34:52 +0500

    Initial Commit

---
+++ ChallengeJSONHWS/ChallengeJSONHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeJSONHWSApp.swift
+//  ChallengeJSONHWS
+//
+//  Created by Baymurat Abdumuratov on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeJSONHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
